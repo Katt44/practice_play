d53d72570340189c9c28b3832badfc93aa1ca2e8
Katt44 <practicalispractical44@gmail.com>
2017-05-30 09:15:14 -0700

    completed first function, added empty functions to complete later

---
+++ lists/iterating.py
@@ -0,0 +1,12 @@
+
+
+
+#### Practicing Iterating thru lists
+
+def meet_the_cats(cats):
+	"""Iterating thru a single list and print the breed of cat"""
+
+	for cat_breed in cats:
+		print "This is cat is an {}. Its damn awesome.".format(cat_breed)
+
+meet_the_cats(["Abyssinian","Aegean","American Bobtail","Arabian Mau"])
