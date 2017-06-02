9b2951fb76cf1e2d24a984e86cc43ce68d91752e
Katt44 <practicalispractical44@gmail.com>
2017-06-02 15:12:47 -0700

    tweaked with numbers and len of word still in progress

--- lists/iterating.py
+++ lists/iterating.py
@@ -10,3 +10,31 @@ def meet_the_cats(cats):
 		print "This is cat is an {}. Its damn awesome.".format(cat_breed)
 
 meet_the_cats(["Abyssinian","Aegean","American Bobtail","Arabian Mau"])
+
+def big_words(words):
+	"""Takes a list of words and appends to new list if word has more than 4 letters"""
+	big_words = []
+	# letter_count = 0
+	for word in words:
+		for letter in word:
+			letter_count = 0
+			letter_count += 1
+			print letter_count
+			
+
+
+def similar_words(word):
+	"""Compares two lists and prints a new list of words in common. """
+	pass
+
+
+def smallest_number(number):
+	"""takes a list of numbers and prints the smallest number  """
+	pass
+
+def largest_number(number):
+	"""takes a list of numbers and prints the largest number  """
+	pass
+
+
+big_words(['cat','hello'])
